dc1c56bac76aabd579a55064297cdd6af2cbb11d
rohit jain <rj33536@gmail.com>
2018-10-20 22:00:49 +0530

    venv

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+pip install  requirements.txt
 
 py manage.py migrate
 
--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rohit\desktop\rj\web-dev\thedjangoapp\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
