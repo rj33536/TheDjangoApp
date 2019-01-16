3f76fb2faadc2072d7de046b063b7fdb66b79dc4
rj33536 <rj33536@gmail.com>
2019-01-16 12:37:11 +0000

    adding particle effect to banner

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,25 +2,41 @@ from django.shortcuts import render
 from .models import product,order
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    context={
+        "products":product.objects.all()[:3]
+    }
+    return render(request,'index.html',context=context)
 def catalog(request):
     context={
         "products":product.objects.all()
     }
-    return render(request,'catalog.html',context=context)    
+    return render(request,'catalog.html',context=context)
 def products(request):
-    return render(request,'product.html')    
+    return render(request,'product.html')
 def FAQ(request):
-    return render(request,'FAQ.html')    
+    return render(request,'FAQ.html')
 def policy(request):
-    return render(request,'policy.html')     
+    return render(request,'policy.html')
 def checkout(request,product_id):
+    context={
+                "error":""
+
+            }
     if(request.method=='POST'):
-        f=order(name=request.POST['name'],Mobile=request.POST['Mobile'],
-                email=request.POST['email'],Address=request.POST['Address'],
-                myproduct=product_id)
-        f.save()
-        print('saved')
+        if(request.POST['name']!='' and request.POST['Mobile']!='' and request.POST['Address']!='' and request.POST['email']!=''  and request.POST['Mobile'].isdigit() ):
+            f=order(name=request.POST['name'],Mobile=request.POST['Mobile'],
+                    email=request.POST['email'],Address=request.POST['Address'],
+                    myproduct=product_id)
+            f.save()
+            context={
+                "error":"order submitted successfully"
+
+            }
+        else:
+            context={
+                "error":"please fill correct information"
+
+            }
 
 
-    return render(request,'order.html')       
+    return render(request,'order.html',context=context)
